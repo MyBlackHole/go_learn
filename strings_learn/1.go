fb5b58d8a08a70e68b77b343e31d2d70a0a6ac04
Black Hole <1358244533@qq.com>
2020-07-05 18:21:22 +0800

    'gee_4'

--- strings_learn/1.go
+++ strings_learn/1.go
@@ -6,11 +6,26 @@ import (
 )
 
 func main() {
+	func2()
+}
+
+type OK struct {
+	a int
+	*int
+}
+
+func func2() {
+	ok := OK{a: 1}
+	i := 0
+	ok.int = &i
+	print(ok.a, ok.int)
+}
+
+func func1() {
 	s := "/black_hole/user/app"
 	sl := strings.Split(s, "/")
 	for _, s := range sl {
 		fmt.Println(s)
 	}
-
 	fmt.Println(s[0])
 }
--- web/gee/gee.go
+++ web/gee/gee.go
@@ -2,39 +2,57 @@ package gee
 
 import "net/http"
 
-// HandlerFunc defines the request handler used by gee
 type HandlerFunc func(*Context)
 
-// Engine implement the interface of ServeHTTP
 type Engine struct {
 	router *router
+	*RouterGroup
+	groups []*RouterGroup
 }
 
-// New is the constructor of gee.Engine
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
+}
+
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
-// GET defines the method to add GET request
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
-// POST defines the method to add POST request
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
-// Run defines the method to start a http server
-func (engine *Engine) Run(addr string) (err error) {
-	return http.ListenAndServe(addr, engine)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
-func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (group *RouterGroup) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, group.engine)
+}
+
+func (group *RouterGroup) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
-	engine.router.handle(c)
+	group.engine.router.handle(c)
 }
