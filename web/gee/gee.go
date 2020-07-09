e8d4c5cea349db9b6677483a80fcbea68c6ba461
Black Hole <1358244533@qq.com>
2020-07-09 23:29:17 +0800

    '静态映射'

--- web/gee/gee.go
+++ web/gee/gee.go
@@ -2,6 +2,7 @@ package gee
 
 import (
 	"net/http"
+	"path"
 	"strings"
 )
 
@@ -69,4 +70,26 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
 	c.handlers = middlewares
 	engine.router.handle(c)
+}
+
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		// Check if file exists and/or if we have permission to access it
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		fileServer.ServeHTTP(c.Writer, c.Req)
+	}
+}
+
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	// Register GET handlers
+	group.GET(urlPattern, handler)
 }
--- web/main2.go
+++ web/main2.go
@@ -3,7 +3,6 @@ package main
 import (
 	"./gee"
 	"log"
-	"net/http"
 	"time"
 )
 
@@ -20,19 +19,20 @@ func onlyForV2() gee.HandlerFunc {
 
 func main() {
 	r := gee.New()
-	r.Use(gee.Logger()) // global midlleware
-	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-	})
-
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2()) // v2 group middleware
-	{
-		v2.GET("/hello/:name", func(c *gee.Context) {
-			// expect /hello/geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-		})
-	}
+	r.Static("/assets", "./web/static")
+	//r.Use(gee.Logger()) // global midlleware
+	//r.GET("/", func(c *gee.Context) {
+	//	c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	//})
+	//
+	//v2 := r.Group("/v2")
+	//v2.Use(onlyForV2()) // v2 group middleware
+	//{
+	//	v2.GET("/hello/:name", func(c *gee.Context) {
+	//		// expect /hello/geektutu
+	//		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	//	})
+	//}
 
 	r.Run(":8008")
 }
