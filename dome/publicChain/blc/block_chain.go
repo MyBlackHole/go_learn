d883fb1c9f177b7800555a6b02c58c977512fbce
black <1358244533@qq.com>
2022-06-19 14:43:31 +0800

    publicChain blockChain.Add

--- dome/publicChain/blc/block_chain.go
+++ dome/publicChain/blc/block_chain.go
@@ -5,6 +5,13 @@ type BlockChain struct {
 	Blocks []*Block
 }
 
+func (blockChain *BlockChain) Add(data string, heigh int64, preHash []byte) {
+	// 创建新区块
+	newBlock := NewBlock(data, heigh, preHash)
+	// 往链里添加区块
+	blockChain.Blocks = append(blockChain.Blocks, newBlock)
+}
+
 // 创建带有创世区块的区块连
 func CreateBlockChainWithGenesisBlock() *BlockChain {
 	genesisBlock := CreateGenesisBlock("Genesis Data")
--- dome/publicChain/main.go
+++ dome/publicChain/main.go
@@ -6,6 +6,15 @@ import (
 )
 
 func main() {
-	genesisBlockChain := blc.CreateBlockChainWithGenesisBlock()
-	fmt.Println(genesisBlockChain)
+	blockChain := blc.CreateBlockChainWithGenesisBlock()
+	fmt.Println(blockChain)
+
+	blockChain.Add("send 100RMB", blockChain.Blocks[len(blockChain.Blocks)-1].Height+1, blockChain.Blocks[len(blockChain.Blocks)-1].Hash)
+	fmt.Println(blockChain)
+
+	blockChain.Add("send 400RMB", blockChain.Blocks[len(blockChain.Blocks)-1].Height+1, blockChain.Blocks[len(blockChain.Blocks)-1].Hash)
+	fmt.Println(blockChain)
+
+	blockChain.Add("send 800RMB", blockChain.Blocks[len(blockChain.Blocks)-1].Height+1, blockChain.Blocks[len(blockChain.Blocks)-1].Hash)
+	fmt.Println(blockChain)
 }
