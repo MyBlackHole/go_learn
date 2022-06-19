27bf11e57c28bd699b921dbe9cdea3c2ce32ddf3
black <1358244533@qq.com>
2022-06-19 15:19:39 +0800

    publicChain 创建新的工作量证明对象

--- dome/publicChain/blc/proof_of_work.go
+++ dome/publicChain/blc/proof_of_work.go
@@ -1,7 +1,14 @@
 package blc
 
+import "math/big"
+
+const TARGETBIT = 16
+
 type ProofOfWork struct {
+	// 需要验证的区块
 	Block *Block
+	// 大数据存储
+	target *big.Int
 }
 
 func (proofOfWork *ProofOfWork) Run() (hash []byte, nonce int64) {
@@ -10,5 +17,9 @@ func (proofOfWork *ProofOfWork) Run() (hash []byte, nonce int64) {
 
 // 创建新的工作量证明对象
 func NewProofOfWork(block *Block) *ProofOfWork {
-	return &ProofOfWork{Block: block}
+	// 创建为 1 的 target
+	target := big.NewInt(1)
+	// 左移 256 - targetBit
+	target = target.Lsh(target, 256-TARGETBIT)
+	return &ProofOfWork{Block: block, target: target}
 }
