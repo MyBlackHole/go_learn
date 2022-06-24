47f32811c768530a9fa4ff813a838596585018fb
black <1358244533@qq.com>
2022-06-24 09:00:34 +0800

    --

--- encoding/xlsx/xuri/excelize/NewFile/main.go
+++ encoding/xlsx/xuri/excelize/NewFile/main.go
@@ -13,38 +13,64 @@ func main() {
 	values := map[string]int{
 		"B2": 2, "C2": 3, "D2": 3, "B3": 5, "C3": 2, "D3": 4, "B4": 6, "C4": 7, "D4": 8}
 	f := excelize.NewFile()
+
+	style, err := f.NewStyle(&excelize.Style{
+		Font: &excelize.Font{
+			Bold:   true,
+			Italic: true,
+			Family: "Times New Roman",
+			Size:   36,
+			Color:  "#777777",
+		},
+	})
+
+	err = f.SetCellStyle("Sheet1", "A1", "H9", style)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = f.SetRowHeight("Sheet1", 2, 50)
+	if err != nil {
+		return
+	}
+
+	err = f.SetColWidth("Sheet1", "A", "A", 10)
+	if err != nil {
+		return
+	}
 	for k, v := range categories {
 		f.SetCellValue("Sheet1", k, v)
 	}
 	for k, v := range values {
 		f.SetCellValue("Sheet1", k, v)
 	}
-	// if err := f.AddChart("Sheet1", "E1", `{
-	//        "type": "col3DClustered",
-	//        "series": [
-	//        {
-	//            "name": "Sheet1!$A$2",
-	//            "categories": "Sheet1!$B$1:$D$1",
-	//            "values": "Sheet1!$B$2:$D$2"
-	//        },
-	//        {
-	//            "name": "Sheet1!$A$3",
-	//            "categories": "Sheet1!$B$1:$D$1",
-	//            "values": "Sheet1!$B$3:$D$3"
-	//        },
-	//        {
-	//            "name": "Sheet1!$A$4",
-	//            "categories": "Sheet1!$B$1:$D$1",
-	//            "values": "Sheet1!$B$4:$D$4"
-	//        }],
-	//        "title":
-	//        {
-	//            "name": "Fruit 3D Clustered Column Chart"
-	//        }
-	//    }`); err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
+	if err := f.AddChart("Sheet1", "E1", `{
+	       "type": "col3DClustered",
+	       "series": [
+	       {
+	           "name": "Sheet1!$A$2",
+	           "categories": "Sheet1!$B$1:$D$1",
+	           "values": "Sheet1!$B$2:$D$2"
+	       },
+	       {
+	           "name": "Sheet1!$A$3",
+	           "categories": "Sheet1!$B$1:$D$1",
+	           "values": "Sheet1!$B$3:$D$3"
+	       },
+	       {
+	           "name": "Sheet1!$A$4",
+	           "categories": "Sheet1!$B$1:$D$1",
+	           "values": "Sheet1!$B$4:$D$4"
+	       }],
+	       "title":
+	       {
+	           "name": "Fruit 3D Clustered Column Chart"
+	       }
+	   }`); err != nil {
+		fmt.Println(err)
+		return
+	}
 	// Save spreadsheet by the given path.
 	if err := f.SaveAs("Book1.xlsx"); err != nil {
 		fmt.Println(err)
