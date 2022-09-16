cba76178e10284455dfca7d5b5cae8508564bef2
black <1358244533@qq.com>
2022-09-17 00:16:13 +0800

    learn

--- go-chi_learn/dome1/go.mod
+++
@@ -1,5 +0,0 @@
-module dome1
-
-go 1.17
-
-require github.com/go-chi/chi v1.5.4
--- go-vgo_learn/robotgo/1/make.sh
+++
@@ -1,5 +0,0 @@
-# 用于交叉编译
-# GOOS=linux GOARCH=amd64 go build -o test
-# GOOS=windows GOARCH=amd64 go build -ldflags="-r ./lib/" -o release/windows/amd64/URun-vpn.exe
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -x ./
-# GOOS=windows GOARCH=386 go build -o release/windows/386/URun-vpn.exe
