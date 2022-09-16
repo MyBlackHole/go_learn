ae48c26f43c61e3c32e6181cc84ac0f70d7539d7
black <1358244533@qq.com>
2022-09-17 00:11:46 +0800

    go-zero

--- go-zero_learn/zero-examples/graceful/dns/api/svc/servicecontext.go
+++
@@ -1,7 +0,0 @@
-package svc
-
-import "github.com/zeromicro/go-zero/zrpc"
-
-type ServiceContext struct {
-	Client zrpc.Client
-}
--- go-zero_learn/zero-examples/load/simulate/cpu/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import _ "github.com/zeromicro/go-zero/core/stat"
-
-func main() {
-	select {}
-}
