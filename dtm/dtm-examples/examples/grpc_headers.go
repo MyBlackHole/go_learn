27a6ea70f108a97a08dd568f6b9011cf7dd76133
black <1358244533@qq.com>
2022-09-17 00:15:21 +0800

    dtm

---
+++ dtm/dtm-examples/examples/grpc_headers.go
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2021 yedf. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+package examples
+
+import (
+	"github.com/dtm-labs/client/dtmcli"
+	"github.com/dtm-labs/client/dtmcli/logger"
+	dtmgrpc "github.com/dtm-labs/client/dtmgrpc"
+	"github.com/dtm-labs/dtm-examples/busi"
+	"github.com/dtm-labs/dtm-examples/dtmutil"
+	"github.com/lithammer/shortuuid/v3"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
+)
+
+func init() {
+	AddCommand("grpc_saga_customHeaders", func() string {
+		req := &busi.ReqGrpc{Amount: 30}
+		gid := shortuuid.New()
+		saga := dtmgrpc.NewSagaGrpc(dtmutil.DefaultGrpcServer, gid).
+			Add(busi.BusiGrpc+"/busi.Busi/TransOutHeaderYes", "", req) // /TransOutHeaderYes will check header exists
+
+		saga.BranchHeaders = map[string]string{
+			"test_header": "test",
+		}
+		saga.WaitResult = true
+		err := saga.Submit()
+		logger.FatalIfError(err)
+		return saga.Gid
+	})
+	AddCommand("grpc_tcc_customHeaders", func() string {
+		gid := shortuuid.New()
+		err := dtmgrpc.TccGlobalTransaction2(dtmutil.DefaultGrpcServer, gid, func(tg *dtmgrpc.TccGrpc) {
+			tg.BranchHeaders = map[string]string{
+				"test_header": "test",
+			}
+			tg.WaitResult = true
+		}, func(tcc *dtmgrpc.TccGrpc) error {
+			data := &busi.ReqGrpc{Amount: 30}
+			r := &emptypb.Empty{}
+			return tcc.CallBranch(data, busi.BusiGrpc+"/busi.Busi/TransOutHeaderYes", "", "", r)
+		})
+		logger.FatalIfError(err)
+		return gid
+	})
+	AddCommand("grpc_saga_passthroughHeaders", func() string {
+		dtmcli.SetPassthroughHeaders([]string{"test_header"})        // set passthrough headers. dtm will
+		dtmgrpc.AddUnaryInterceptor(busi.SetGrpcHeaderForHeadersYes) // will set header in this middleware
+
+		req := &busi.ReqGrpc{Amount: 30}
+		gid := shortuuid.New() + "HeadersYes" // gid with this post fix will be handled in interceptor
+		saga := dtmgrpc.NewSagaGrpc(dtmutil.DefaultGrpcServer, gid).
+			Add(busi.BusiGrpc+"/busi.Busi/TransOutHeaderYes", "", req) // /TransOutHeaderYes will check header exists
+
+		saga.WaitResult = true
+		err := saga.Submit()
+		logger.FatalIfError(err)
+		return saga.Gid
+	})
+}
---
+++ dtm/gozerodtm/order-srv/internal/logic/createLogic.go
@@ -0,0 +1,64 @@
+package logic
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/tal-tech/go-zero/core/stores/sqlx"
+	"gozerodtm/order-srv/internal/model"
+	"gozerodtm/order-srv/internal/svc"
+	"gozerodtm/order-srv/pb"
+
+	"github.com/yedf/dtmgrpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/tal-tech/go-zero/core/logx"
+)
+
+type CreateLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewCreateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CreateLogic {
+	return &CreateLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *CreateLogic) Create(in *pb.CreateReq) (*pb.CreateResp, error) {
+
+	fmt.Printf("创建订单 in : %+v \n", in)
+
+	//barrier防止空补偿、空悬挂等具体看dtm官网即可，别忘记加barrier表在当前库中，因为判断补偿与要执行的sql一起本地事务
+	barrier, err := dtmgrpc.BarrierFromGrpc(l.ctx)
+	db, err := sqlx.NewMysql(l.svcCtx.Config.DB.DataSource).RawDB()
+
+	if err != nil {
+		//!!!一般数据库不会错误不需要dtm回滚，就让他一直重试，这时候就不要返回codes.Aborted, dtmcli.ResultFailure 就可以了，具体自己把控!!!
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	if err := barrier.CallWithDB(db, func(tx *sql.Tx) error {
+
+		order := new(model.Order)
+		order.GoodsId = in.GoodsId
+		order.Num = in.Num
+		order.UserId = in.UserId
+
+		_, err = l.svcCtx.OrderModel.Insert(tx, order)
+		if err != nil {
+			return fmt.Errorf("创建订单失败 err : %v , order:%+v \n", err, order)
+		}
+
+		return nil
+	}); err != nil {
+		//!!!一般数据库不会错误不需要dtm回滚，就让他一直重试，这时候就不要返回codes.Aborted, dtmcli.ResultFailure 就可以了，具体自己把控!!!
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &pb.CreateResp{}, nil
+}
